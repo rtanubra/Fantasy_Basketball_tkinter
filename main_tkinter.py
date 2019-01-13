73c8d523130861536a0b8b7109d72986273c623f
rtanubra <rdtanubrata@gmail.com>
2019-01-13 14:14:06 -0500

    tkinter able to receive input from user

--- main_tkinter.py
+++ main_tkinter.py
@@ -12,7 +12,17 @@ class Application(tk.Frame):
     
     def submit(self):
         print("Initializing Trade Analysis. Please wait:")
-
+        my_names = []
+        if self.player1_inp.get():
+            my_names.append(self.player1_inp.get())
+        if self.player2_inp.get():
+            my_names.append(self.player2_inp.get())
+        if self.player3_inp.get():
+            my_names.append(self.player3_inp.get())
+        if len(my_names) < 2:
+            print()
+        print(my_names)
+    
     def create_widgets(self):
         self.title = tk.Label(self)
         self.title["text"] = "Fantasy NBA Analyst\nLet's Begin"
@@ -28,14 +38,22 @@ class Application(tk.Frame):
         self.player2_inp = tk.Entry(self)
         self.player2_inp.grid(row=2,column=2)
 
+        self.player3_label= tk.Label(self,text="Player 3")
+        self.player3_label.grid(row=3,column=0)
+        self.player3_inp = tk.Entry(self)
+        self.player3_inp.grid(row=3, column=2)
+
+        self.status_label = tk.Label(self,text="Prepared to Analyze provide at least 2 players to compare")
+        self.status_label.grid(row=4,columnspan=3)
+
         self.submit = tk.Button(
             self, text = "SUBMIT", fg= "green",command = self.submit
         )
-        self.submit.grid(row=3,column=2)
+        self.submit.grid(row=5,column=2)
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
-        self.quit.grid(row=4)
+        self.quit.grid(row=6)
 
     
 
