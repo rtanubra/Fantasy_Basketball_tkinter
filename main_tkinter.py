3866a0090a9914953bab1a5e059f7334c6a03cef
rtanubra <rdtanubrata@gmail.com>
2019-01-13 14:50:53 -0500

    First commit to pack. Things don't look quite right

--- main_tkinter.py
+++ main_tkinter.py
@@ -39,38 +39,39 @@ class Application(tk.Frame):
     def create_widgets(self):
         self.title = tk.Label(self)
         self.title["text"] = "Fantasy NBA Analyst\nLet's Begin"
-        self.title.grid(columnspan=4)
+        self.title.pack(side="top",fill="x")
         
         self.player1_label =tk.Label(self,text="Player 1")
-        self.player1_label.grid(row=1,column=0)
+        self.player1_label.pack()
         self.player1_inp = tk.Entry(self)
-        self.player1_inp.grid(row=1,column=2)
+        self.player1_inp.pack(side="left")
        
         self.player2_label =tk.Label(self,text="Player 2")
-        self.player2_label.grid(row=2,column=0)
+        self.player2_label.pack()
         self.player2_inp = tk.Entry(self)
-        self.player2_inp.grid(row=2,column=2)
+        self.player2_inp.pack()
 
         self.player3_label= tk.Label(self,text="Player 3")
-        self.player3_label.grid(row=3,column=0)
+        self.player3_label.pack()
         self.player3_inp = tk.Entry(self)
-        self.player3_inp.grid(row=3, column=2)
+        self.player3_inp.pack()
 
         self.submit = tk.Button(
             self, text = "SUBMIT", fg= "green",command = self.submit
         )
-        self.submit.grid(row=5,column=2) 
+        self.submit.pack()
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
-        self.quit.grid(row=6)
+        self.quit.pack()
 
         #******* STATUS BAR *******
         self.status = tk.Label(self,text="Ready to Analyze",bd=1,relief="sunken",anchor="w")
-        self.status.grid(row=8)
+        self.status.pack(side="bottom",fill="x")
 
     
 
 root = tk.Tk()
+root.minsize(width=350, height=250)
 app = Application(master=root)
 app.mainloop()
