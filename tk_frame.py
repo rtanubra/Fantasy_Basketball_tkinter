56d866e2fef10d5651feca0876bf97bc5fd31630
rtanubra <rdtanubrata@gmail.com>
2019-01-13 19:47:20 -0500

    Built Top Frame in tkinter

--- tk_frame.py
+++ tk_frame.py
@@ -10,26 +10,72 @@ from ohmysportsfeedspy import MySportsFeeds
 
 
 class MainApplication(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-
-        self.Top = tk.Frame(self,width=1100,height=90,bg="powder blue", relief="sunken")
-        self.Top.pack(side="top")
-
-        self.left = tk.Frame(self,width=440,height=245,bg="red", relief="sunken")
-        self.left.pack(side="left")
+    def __init__(self, master=None):
+        super().__init__(master)
+        #initiate tk functions you will use here.
+        self.master = master
         
-        self.right = tk.Frame(self,width=640,height=245,bg="red", relief="sunken")
-        self.right.pack(side="right")   
+        self.Tops = tk.Frame(self.master,width=1200,height=200,bg="powder blue",relief="sunken")
+        self.Tops.pack(side="top",fill="x")
 
-        """
-        self.bottom = tk.Frame(self,width=1100,height=345,bg="powder blue", relief="sunken")
-        self.bottom.pack(side="bottom")
-        """
+        self.f1 = tk.Frame(self.master,width=550,height=500,bg="powder blue",relief="sunken")
+        self.f1.pack(side="left")
+
+        self.f2 = tk.Frame(self.master,width=550,height=500,bg="powder blue",relief="sunken")
+        self.f2.pack(side="right")
+        
+        self.create_widgets_Tops(self.Tops)
+        self.create_widgets_f1(self.f1)
+        self.create_widgets_f2(self.f2)
+
+#==========================TOP is name input========================
+    def create_widgets_Tops(self,frame):
+        #this will create widgets for name inputs
+        self.title = tk.Label(frame,text="Input player names for analysis",fg="black",bd=10,anchor="w",font=("ariel",20,"bold"))
+        self.title.grid(row=0,column=3)
+
+        self.player1_label =tk.Label(frame,text="Player 1")
+        self.player1_label.grid(row=1,column=0)
+        self.player1_inp = tk.Entry(frame)
+        self.player1_inp.grid(row=1,column=1)
+       
+        self.player2_label =tk.Label(frame,text="Player 2")
+        self.player2_label.grid(row=2,column=0)
+        self.player2_inp = tk.Entry(frame)
+        self.player2_inp.grid(row=2,column=1)
+
+        self.player3_label= tk.Label(frame,text="Player 3")
+        self.player3_label.grid(row=3,column=0)
+        self.player3_inp = tk.Entry(frame)
+        self.player3_inp.grid(row=3, column=1)
+
+        self.submit = tk.Button(
+            frame, text = "SUBMIT", fg= "green",command = self.submit
+        )
+        self.submit.grid(row=3,column=2) 
+
+        self.quit = tk.Button(frame, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.grid(row=4,column=2)
+
+#==========================LEFT f1 category breakdown========================   
+    def create_widgets_f1(self,frame1):
+        #this will create widgets to compare category stuff
+        pass
+    
+#==========================RIGHT f2 contribution points breakdown======================== 
+    def create_widgets_f2(self,frame2):
+        #this will create widgets to compare contribution points
+        pass
+        
+#===================================SUBMIT FUNCTION============================
+    def submit(self):
+        print("Initializing Trade Analysis. Please wait:")
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("1100x700+0+0")
+    root.title("NBA Fantasy Analysis App")
+    root.geometry("1200x750+0+0")
     MainApplication(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
